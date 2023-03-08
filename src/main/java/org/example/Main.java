936fc85dddf1e4702eeb5a25c6eec8776e9488ac
깃ohtj6644 <ohtj6644@naver.com>
2023-03-08 15:31:18 +0900

    ãhomeStudy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("0308 홈스터디 셋팅 ");
     }
 }
